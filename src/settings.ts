075802cd1a1d0b46496a03535fed9ddfffa49e0a
yuraovdnk <yuraovdnk1999@ukr.net>
2022-06-23 09:44:28 +0300

    fix mongoUrl

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,5 @@
 export const settings = {
     JWT_SECRET: process.env.JWT_SECRET || "123",
-    MONGODB:'mongodb://localhost:27017' || 'mongodb+srv://yuraovdnk:ovdey1970@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority'
+    MONGODB: 'mongodb+srv://yuraovdnk:ovdey1970@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority'
 }
+//'mongodb://localhost:27017'
