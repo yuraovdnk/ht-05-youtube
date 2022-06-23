06bef5689271f0118935cf1b9baf26f5a499bd87
yuraovdnk <yuraovdnk1999@ukr.net>
2022-06-23 10:23:58 +0300

    fix mongoUrl

--- src/routes/testing-route.ts
+++ src/routes/testing-route.ts
@@ -10,9 +10,16 @@ import {
 export const testingRoute = Router()
 
 testingRoute.delete('/all-data',async (req: Request, res: Response)=>{
-    await bloggerCollection.deleteMany({})
-    await postsCollection.deleteMany({})
-    await usersCollection.deleteMany({})
-    await commentsCollection.deleteMany({})
-    await ipAddressesCollection.deleteMany({})
+
+    try {
+        await bloggerCollection.deleteMany({})
+        await postsCollection.deleteMany({})
+        await usersCollection.deleteMany({})
+        await commentsCollection.deleteMany({})
+        await ipAddressesCollection.deleteMany({})
+        res.sendStatus(200)
+    }
+    catch (e){
+        console.log(e)
+    }
 })
